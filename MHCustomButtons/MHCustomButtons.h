0f93a14ab826c5deb88e9cedfbd8b95abed19c1d
Maiko Hermans <mwmhermans@outlook.com>
2017-03-20 10:35:54 +0100

    Initial Commit

---
+++ MHCustomButtons/MHCustomButtons.h
@@ -0,0 +1,19 @@
+//
+//  MHCustomButtons.h
+//  MHCustomButtons
+//
+//  Created by Maiko Hermans on 20/03/2017.
+//  Copyright © 2017 Maiko Hermans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHCustomButtons.
+FOUNDATION_EXPORT double MHCustomButtonsVersionNumber;
+
+//! Project version string for MHCustomButtons.
+FOUNDATION_EXPORT const unsigned char MHCustomButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHCustomButtons/PublicHeader.h>
+
+
